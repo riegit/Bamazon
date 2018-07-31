3dcab88445ce6e8d39ea0859617e8114785f5706
Tarante <37225235+riegit@users.noreply.github.com>
2018-07-30 22:24:46 -0400

    added #2 assignment bamazonManager.js

---
+++ bamazonManager.js
@@ -0,0 +1,163 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+       host:'localhost',
+       port:'3306',
+       user:'root',
+       password:'root',
+       database:'bamazon'
+});
+connection.connect(function(err){
+    if(err) throw err;
+    start();
+})
+var start = function(){
+    inquirer.prompt({
+        name:'action',
+        type : 'list',
+        choices : [
+            'View Products for Sale',
+            'View Low Inventory',
+            'Add to Inventory',
+            'Add New Product' ,
+            'End Session'
+        ],
+        message:'What would you like to do?'
+    }).then(function(ans){
+        switch(ans.action){
+            case 'View Products for Sale': viewProducts();
+            break;
+            case 'View Low Inventory': viewInventory();
+            break;
+            case 'Add to Inventory': addInventory();
+            break;
+            case 'Add New Product': addNewProduct();
+            break;
+            case 'End Session': console.log('Good Bye...')
+        }
+    });
+}
+var viewProducts = function(){
+    console.log('\n---- View All Inventory ----\n\n');
+    
+    connection.query('SELECT * FROM products', function(err, res){
+        if(err) throw err;
+      
+        for(var i = 0; i<res.length;i++){
+            console.log('Item id: ' + res[i].item_id,' | Product Name: ' + res[i].product_name + ' | Department: '+ res[i].department_name + ' | Price: ' + res[i].price + ' | Stocks: ' + res[i].stock_quantity) ;
+            console.log('------------------------------------------------------------------------------------------------------------------')
+        }        
+        start();
+    });
+}
+var viewInventory = function(){
+    console.log('\n----  View Low inventory items ----\n\n');
+
+    connection.query('SELECT * FROM products', function(err,res){
+        if(err) throw err;
+
+        for (var i=0; i < res.length; i++){
+            if(res[i].stock_quantity <= 5){
+                
+            console.log('Item id: ' + res[i].item_id,' | Product Name: ' + res[i].product_name + ' | Department: '+ res[i].department_name + ' | Price: ' + res[i].price + ' | Stocks: ' + res[i].stock_quantity) ;
+            console.log('------------------------------------------------------------------------------------------------------------------')  
+            }   
+        } 
+        start();  
+    });  
+}
+var addInventory = function(){
+    console.log('\n----  Add to inventory ----\n\n');
+    connection.query('SELECT * FROM products ', function(err,res){
+        inquirer.prompt([
+            {
+            name:'id',
+            type:'input',
+            message:'Please enter the product id you wish to add inventory', 
+            validate:function(value){
+                if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+                    return true;
+                }else {
+                    return false;
+                }        
+            }
+            },{
+            name:'quantity',
+            type:'input',
+            message:'How many items would you like to add to the product?', 
+            validate:function(value){
+                if(isNaN(value) == false){
+                    return true;
+                }else {
+                    return false;
+                }     
+            }
+        }]).then(function(ans){
+            var inventoryToAdd = (ans.id)-1;
+            var howManyToAdd = parseInt(ans.quantity);           
+            connection.query('UPDATE products SET ? WHERE ?',[
+                {stock_quantity:(res[inventoryToAdd].stock_quantity+howManyToAdd)},
+                {item_id:ans.id}, 
+                //Make sure NOT to use 'res' here otherwise an error 'undefined'occurs   
+            ],function(err,result){
+                console.log('\n----- The quantity has been updated ---\n');
+                start();
+            });                         
+        });
+    });                       
+}
+
+var addNewProduct = function(){
+    console.log('\n----  Add new product ----\n');
+    var deptArr=[];
+    connection.query('SELECT * FROM departments', function(err,res){
+        if(err) throw err;
+        for (var i=0; i < res.length; i++){
+            deptArr.push(res[i].department_name);
+        }
+    })
+    inquirer.prompt([
+        {
+        name:'product',
+        type:'input',
+        message:'Product Name: ',  
+        validate:function(value){
+        if(value){return true;}
+        else{return false;}
+        }      
+    },{
+        name:'department',
+        type:'list',
+        choices:deptArr,
+        message:'Department: ',
+    },{
+        name:'price',
+        type:'input',
+        message:'Price: ',
+        validate: function(value){
+        if(isNaN(value) === false){return true;}
+        else{return false;}
+        }
+    },{
+        name:'qty',
+        type:'input',
+        message:'Quantity: ',
+        validate: function(value){
+        if(isNaN(value) === false){return true;}
+        else{return false;}
+        }
+    },
+    ]).then(function(ans){
+        connection.query('INSERT INTO products SET ? ' ,{
+            product_name:ans.product,
+            department_name:ans.department,
+            price:ans.price,
+            stock_quantity:ans.qty,         
+        },function(err,result){
+            if(err) throw err;
+            console.log('\n---- New product has been added ----\n');
+            start();
+        });             
+    });
+}
--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ USE bamazon;
 
 CREATE TABLE products(
 	item_id INT AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(100) NOT NULL,
+    product_name VARCHAR(200) NOT NULL,
     department_name VARCHAR(50) ,
     price DECIMAL(10,2),
     stock_quantity INT(10) NOT NULL,
@@ -19,6 +19,24 @@ VALUES ('Espresso Maker','HOME & KITCHEN',125.45,32),
     ('27inch HD Monitor','Electronics',159.00,17),
     ('Solar Lights Outdoor','Outdoor & Garden',23.96,42),
     ('Wooden Handle Tool Set','Outdoor & Garden',19.99,31),
-    ('Outdoor Dining Set','Outdoor & Garden',575.99,9),
-    ('Japanese Matcha Whisk','HOME & KITCHEN',13.55,16),
-    ('Iced Coffee and Tea Brewing System','HOME & KITCHEN',34.55,27);
+    ('Outdoor Dining Set','Outdoor & Garden',575.99,1),
+    ('Japanese Matcha Whisk','HOME & KITCHEN',13.55,3),
+    ('Iced Coffee and Tea Brewing System','HOME & KITCHEN',34.55,30);
+    
+    
+CREATE TABLE departments(
+department_id INT AUTO_INCREMENT NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+over_head_costs DECIMAL(10,2) NOT NULL,
+total_sales DECIMAL(10,2) NOT NULL,
+PRIMARY KEY(department_id)
+);
+    
+INSERT INTO departments(department_name, over_head_costs, total_sales)
+VALUES ('ENTERTAINMENT', 50000.00, 15000.00),
+    ('ELECTRONICS', 20000.00, 12000.00),
+    ('HOME & KITCHEN', 30000.00, 15000.00),
+    ('BODY & HEALTH', 3000.00, 12000.00),
+    ('BOOKs & Musics', 40000.00, 12000.00),
+    ('CLOTHING', 35000.00, 15000.00),
+    ('Outdoor & Garden', 12000.00, 12000.00);
